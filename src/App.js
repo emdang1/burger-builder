dc7a47740fa8f8046c66811ce1690f7bd4292eb8
dangm <dang.m@seznam.cz>
2020-04-01 15:55:23 +0200

    added Checkout and CheckoutSummary components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from './Components/Layout/Layout';
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
 import './App.css';
+import Checkout from './Containers/Checkout/Checkout';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <div>
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
