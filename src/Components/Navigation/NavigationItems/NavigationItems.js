acce221c8781e7fe641163fb1aeacef0171245ba
dangm <dang.m@seznam.cz>
2020-04-17 01:18:14 +0200

    added switching navigation item from authenticate to logout, if logged in

--- src/Components/Navigation/NavigationItems/NavigationItems.js
+++ src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -2,17 +2,27 @@ import React from 'react';
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
+import { connect } from 'react-redux';
+
 const navigation = (props) => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link='/' exact>
       Burger Builder
     </NavigationItem>
     <NavigationItem link='/orders'>Orders</NavigationItem>
-    <NavigationItem link='/auth'>Authenticate</NavigationItem>
+    {props.isAuthenticated ? (
+      <NavigationItem link='/logout'>Logout</NavigationItem>
+    ) : (
+      <NavigationItem link='/auth'>Authenticate</NavigationItem>
+    )}
   </ul>
 );
 
-export default navigation;
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.userId != null,
+});
+
+export default connect(mapStateToProps)(navigation);
 
 // "active" can be left like this - because its boolean value
 // active={true} is the same thing
