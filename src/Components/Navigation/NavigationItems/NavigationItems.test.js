52f4b21be6d59af772637e40f666288c2fc0ea1f
dangm <dang.m@seznam.cz>
2020-04-20 15:24:23 +0200

    second test, using beforeEach and wrapper.setProps

--- src/Components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/Components/Navigation/NavigationItems/NavigationItems.test.js
@@ -11,8 +11,21 @@ configure({ adapter: new Adapter() });
 // setting up test bundle with "describe"
 // seting up test case with "it"
 describe('<NavigationItems/>', () => {
+  let wrapper;
+  // is called before each test
+  beforeEach(() => {
+    wrapper = shallow(<Navigation />);
+  });
+
   it('should render two <NavigationItem/> if unauthenticated', () => {
-    const wrapper = shallow(<Navigation />);
+    // const wrapper = shallow(<Navigation />); --- can be deleted due to the beforeEach
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  // passing prop "isAuthenticated" as true to the <Navigation/> component
+  it('should render three <NavigationItem/> if authenticated', () => {
+    // const wrapper = shallow(<Navigation isAuthenticated />); --- can be deleted due to the beforeEach
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
