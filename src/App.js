9f35736daee71b510df9977f32363db0a2304636
dangm <dang.m@seznam.cz>
2020-04-21 11:38:38 +0200

    all other routes besides burgerBuilder now lazy loaded with suspense, suspense wrapping whole switch, using normal component prop no render, fallback of suspense is spinner

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Suspense } from 'react';
 import Layout from './Components/Layout/Layout';
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
-import Orders from './Containers/Orders/Orders';
 import './App.css';
-import Checkout from './Containers/Checkout/Checkout';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import Logout from './Containers/Auth/Logout/Logout';
 import { connect } from 'react-redux';
 import { tryAuthCheck } from './store/actions/auth';
+import Spinner from './Components/UI/Spinner/Spinner';
 
-// this import is not needed anymore due to react.lazy dynamic import (works only with default exports for now)
+// these imports arent needed anymore due to react.lazy dynamic imports (works only with default exports for now)
+
+// import Orders from './Containers/Orders/Orders';
+// import Checkout from './Containers/Checkout/Checkout';
 // import Auth from './Containers/Auth/Auth';
+// import Logout from './Containers/Auth/Logout/Logout';
+
 const Auth = React.lazy(() => import('./Containers/Auth/Auth'));
+const Checkout = React.lazy(() => import('./Containers/Checkout/Checkout'));
+const Logout = React.lazy(() => import('./Containers/Auth/Logout/Logout'));
+const Orders = React.lazy(() => import('./Containers/Orders/Orders'));
 
 class App extends React.Component {
   componentDidMount() {
@@ -22,37 +28,40 @@ class App extends React.Component {
     // unauthenticated routes
     // redirect for "unknown" or every other routes
 
-    // using suspense to define the lazily loading component
-    // thats the previously setup variable with react.lazy
+    // L A Z Y   L O A D I N G
+    // using suspense to define the lazily loaded component
+    // thats the previously set variable with react.lazy
     // suspense needs fallback, something like loading, meanwhile it loads
     // since we are using "render" and not "component" we have to pass props and spread them onto the component
+
+    // we can wrap the whole swith into suspense
+    // then we can normally use component and no render with props spreading
+
+    // you can also
     let routes = (
-      <Switch>
-        <Route
-          path='/auth'
-          render={(props) => (
-            <Suspense fallback={<div>Loading... </div>}>
-              <Auth {...props} />
-            </Suspense>
-          )}
-        />
-        <Route path='/' exact component={BurgerBuilder} />
-        <Redirect to='/' />
-      </Switch>
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route path='/auth' component={Auth} />
+          <Route path='/' exact component={BurgerBuilder} />
+          <Redirect to='/' />
+        </Switch>
+      </Suspense>
     );
 
     // authenticated routes
     // redirect for "unknown" or every other routes
     if (this.props.isAuthenticated) {
       routes = (
-        <Switch>
-          <Route path='/auth' component={Auth} />
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
-          <Route path='/logout' component={Logout} />
-          <Route path='/' exact component={BurgerBuilder} />
-          <Redirect to='/' />
-        </Switch>
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route path='/auth' component={Auth} />
+            <Route path='/checkout' component={Checkout} />
+            <Route path='/orders' component={Orders} />
+            <Route path='/logout' component={Logout} />
+            <Route path='/' exact component={BurgerBuilder} />
+            <Redirect to='/' />
+          </Switch>
+        </Suspense>
       );
     }
     return (
