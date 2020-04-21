91d4ec51eb1bc6163d8c82332f42d693c0bded77
dangm <dang.m@seznam.cz>
2020-04-21 10:53:26 +0200

    added first lazily loaded component with react.lazy and suspense - render method

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Layout from './Components/Layout/Layout';
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
 import Orders from './Containers/Orders/Orders';
 import './App.css';
 import Checkout from './Containers/Checkout/Checkout';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import Auth from './Containers/Auth/Auth';
 import Logout from './Containers/Auth/Logout/Logout';
 import { connect } from 'react-redux';
 import { tryAuthCheck } from './store/actions/auth';
+
+// this import is not needed anymore due to react.lazy dynamic import (works only with default exports for now)
+// import Auth from './Containers/Auth/Auth';
+const Auth = React.lazy(() => import('./Containers/Auth/Auth'));
 
 class App extends React.Component {
   componentDidMount() {
@@ -18,9 +21,21 @@ class App extends React.Component {
   render() {
     // unauthenticated routes
     // redirect for "unknown" or every other routes
+
+    // using suspense to define the lazily loading component
+    // thats the previously setup variable with react.lazy
+    // suspense needs fallback, something like loading, meanwhile it loads
+    // since we are using "render" and not "component" we have to pass props and spread them onto the component
     let routes = (
       <Switch>
-        <Route path='/auth' component={Auth} />
+        <Route
+          path='/auth'
+          render={(props) => (
+            <Suspense fallback={<div>Loading... </div>}>
+              <Auth {...props} />
+            </Suspense>
+          )}
+        />
         <Route path='/' exact component={BurgerBuilder} />
         <Redirect to='/' />
       </Switch>
